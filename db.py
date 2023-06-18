8f047de840da57bf64852a7a72ec3ed5326146c1
pmcu <pmcu@users.noreply.github.com>
2023-06-18 16:30:12 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,11 @@
+import sqlite3
 
+conn = sqlite3.connect("testdb.db")
+
+cur = cur conn.cursor()
+cur.execute('''
+CREATE TABLE IF NOT EXIST people
+(first_name TEXT, last_name Text)''')
+conn.commit()
+cur.close()
+conn.close()
