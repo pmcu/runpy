a1bf9f9b1dae0fcff26a5518b1197445e43b2e82
pmcu <pmcu@users.noreply.github.com>
2023-06-18 13:49:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import datetime as dt
-import pandas as pd
+#import pandas as pd
 print("Hello, World! and Dogs")
-def show_time():
-    Element("today").write(str(dt.date.today()))
-    print(dt.date.today())
-show_time()
+
 
 list =[
     ['dog','brown','Touser'],
@@ -12,8 +9,8 @@ list =[
     ['cat','brown','Typhus'],
     ['dog','black','Touser'],
 ]
-df = pd.DataFrame(list)
+#df = pd.DataFrame(list)
 
 def showlist():
-    Element("lists").write(df)
+    Element("lists").write(list)
 #showlist() 
