bcdafb6aac2243ac09590439326baca64a1a34ff
pmcu <pmcu@users.noreply.github.com>
2023-06-18 16:55:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import pandas as pd
 list =[
     ['dog','brown','Touser'],
     ['donkey','brown','Toofus'],
     ['cat','brown','Typhus'],
     ['dog','black','Touser'],
 ]
+'''
 for item in list:
     print(item)
+'''
+
+mydataset = {
+'cars': ["BMW","Volvo","Ford"],
+'passings': [3,7,2]
+    }
+myvar = pd.DataFrame(mydatset)
+print(myvar)
 
