c20c0715abc7c21c4603ef6a5490e9d3de20590c
pmcu <pmcu@users.noreply.github.com>
2023-06-18 15:32:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import datetime as dt
-import pandas as pd
-#print("Hello, World! and Dogs")
-
-
 list =[
     ['dog','brown','Touser'],
     ['donkey','brown','Toofus'],
     ['cat','brown','Typhus'],
     ['dog','black','Touser'],
 ]
-df = pd.DataFrame(list)
+for item in list:
+    print(item)
 
-print(df) 
